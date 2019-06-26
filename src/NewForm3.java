55fa8b722268b3a8dcd4652878351f36692f0ca0
ttp <1877784332@qq.com>
2019-06-26 09:54:26 +0800

    完成 TODO2

--- src/NewForm3.java
+++ src/NewForm3.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,16 +17,17 @@ public class NewForm3 extends JFrame {
     private textAreaRes3 jToolBarRes = new textAreaRes3("创建表并生成随机数据", "修改下方文本框执行SQL");
     private JTextArea textAreaIn = new JTextArea();
     private JTextArea textAreaOut = new JTextArea();
-    private Object[][] obj = {{""}, {""}, {""}, {""}, {""}};
+    private Object[][] obj = {{""}, {""}, {""}, {""}, {""}, {""}, {""}};
     private String[] columnNames = {"col1", "col2", "col3", "col4"};
     public JTable table = new JTable();
     public JTable tableRes = new JTable();
+    private int realCol = 0;
 
     NewForm3() throws Exception {
         // ---------------------------------------------主窗口设置
         setTitle("SQL快速验证工具");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setSize(1200, 618);
+        setSize(1200, 800);
 
         // ----------------------------------------------table设置
         table.setModel(new DefaultTableModel(obj, columnNames));
@@ -48,7 +51,7 @@ public class NewForm3 extends JFrame {
             firsetColumn.setMinWidth(200);
         }
         tableRes.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-
+        textAreaIn.setFont(new Font("宋体", Font.BOLD, 30));
         textAreaOut.setFont(new Font("宋体", Font.BOLD, 30));
         JScrollPane js = new JScrollPane(textAreaOut);
         js.setHorizontalScrollBarPolicy(
@@ -65,6 +68,7 @@ public class NewForm3 extends JFrame {
         JSplitPane jSplitPane2 = new JSplitPane(JSplitPane.VERTICAL_SPLIT, jSplitPane1, js);
         JSplitPane jSplitPane3 = new JSplitPane(JSplitPane.VERTICAL_SPLIT, table, tableRes);
         JSplitPane jSplitPaneRes = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, jSplitPane3, jSplitPane2);
+        jSplitPane3.setResizeWeight(0.5);
         jSplitPane1.setResizeWeight(0.1);
         jSplitPane2.setResizeWeight(0.5);
         jSplitPaneRes.setResizeWeight(0.9);
@@ -81,6 +85,11 @@ public class NewForm3 extends JFrame {
         Statement st = c.createStatement();
 
         jToolBarRes.butRes1.addActionListener(e -> {
+            try {
+                st.execute("drop table t");
+            } catch (SQLException ex) {
+                System.out.print("第一次drop");
+            }
             System.out.println("第二步 执行创建表语句");
             StringBuilder sql = new StringBuilder();
             // 表名为t
@@ -88,11 +97,13 @@ public class NewForm3 extends JFrame {
             for (int i = 0; i < table.getColumnCount(); i++) {
                 if (i == 0) {
                     sql.append(((String) table.getValueAt(0, i))).append("\n");
+                    realCol++;
                     continue;
                 }
                 if (null == table.getValueAt(0, i)) {
                     continue;
                 }
+                realCol++;
                 sql.append(",").append(((String) table.getValueAt(0, i))).append("\n");
             }
             sql.append(");\n");
@@ -143,21 +154,41 @@ public class NewForm3 extends JFrame {
                     ex.printStackTrace();
                 }
             }
-            // TODO 2 执行输入框的SQL数据   一般用SELECT 方便测试SQL语句
+            // 2 执行输入框的SQL数据   一般用SELECT 方便测试SQL语句
             if ((textAreaIn == null) || ("".equals(textAreaIn.getText()))) {
                 return;
             }
             try {
-                ResultSet rs = st.executeQuery(textAreaOut.getText());
+                ResultSet rs = st.executeQuery(textAreaIn.getText());
+                // TODO 3 根据返回结果，更新下面的表格
+                int j = 0;
                 while (rs.next()) {
-                    String name = rs.getString(1);
-                    System.out.println(name);
+                    j++;
+                    for (int i = 1; i <= realCol; i++) {
+                        String name = rs.getString(i);
+                        tableRes.setValueAt(name, j, i - 1);
+                    }
                 }
             } catch (SQLException ex) {
                 textAreaOut.append("SQL语句出错\n");
                 ex.printStackTrace();
             }
-            // TODO 3 根据返回结果，更新下面的表格
+
+        });
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.out.println("drop table t");
+                try {
+                    st.execute("drop table t");
+                    if (st != null)
+                        st.close();
+                    c.close();
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            }
         });
     }
 }
